3eb218c7ce41d515ec997c9d69af717452df635a
Kenny Vu <kennyvu@live.ca>
2020-08-18 12:07:04 -0400

    Exercise 2.3 done

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -11,15 +11,14 @@ const options = {
 };
 
 const createGreeting = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
   try {
-    const client = await MongoClient(MONGO_URI, options);
-    await client.connect();
-
     const db = client.db("exercise-1");
 
     const r = await db.collection("greetings").insertOne(req.body);
     assert.equal(1, r.insertedCount);
-    if (r) {
+    if (r.insertedCount === 1) {
       res.status(201).json({ status: 201, data: req.body });
     } else
       res
@@ -31,4 +30,17 @@ const createGreeting = async (req, res) => {
   client.close();
 };
 
-module.exports = { createGreeting };
+const getGreeting = async (req, res) => {
+  const _id = req.params._id;
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("exercise-1");
+  db.collection("greetings").findOne({ _id }, (err, result) => {
+    result
+      ? res.status(200).json({ status: 200, _id, data: result })
+      : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    client.close();
+  });
+};
+
+module.exports = { createGreeting, getGreeting };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
 const { addUser } = require("./exercises/exercise-1.4");
-const { createGreeting } = require("./exercises/exercise-2");
+const { createGreeting, getGreeting } = require("./exercises/exercise-2");
 
 const PORT = process.env.PORT || 8000;
 
@@ -19,7 +19,8 @@ express()
   // exercise 1.3
   //Tried to make this endpoint more dynanmic using a req.param. Don't know if this is good practice.
   .get("/:dbName/users", getUsers)
-
+  //exercise 2.3
+  .get("/greeting/:_id", getGreeting)
   //exercise 1.4
   .post("/exercise-1/users", addUser)
   //exercise 2.1
